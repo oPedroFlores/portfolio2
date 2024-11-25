6c88c65496ec32bac7d417232499b77e5cbf5524
Pedro Flores <pedrolucaspflores@gmail.com>
2024-11-25 20:38:40 -0300

    project: add venus marketplace

--- src/img/projectImages.js
+++ src/img/projectImages.js
@@ -1,9 +1,10 @@
-import image1 from "../img/projectImages/myfriends.gif";
-import image2 from "../img/projectImages/blognode.png";
-import image3 from "../img/projectImages/crudreactnode.png";
-import image4 from "../img/projectImages/simple-logineregister.png";
-import image5 from "../img/projectImages/bike-landingpage.png";
-import image6 from "../img/projectImages/pxghelper.png";
+import image1 from '../img/projectImages/myfriends.gif';
+import image2 from '../img/projectImages/blognode.png';
+import image3 from '../img/projectImages/crudreactnode.png';
+import image4 from '../img/projectImages/simple-logineregister.png';
+import image5 from '../img/projectImages/bike-landingpage.png';
+import image6 from '../img/projectImages/pxghelper.png';
+import image7 from '../img/projectImages/venus.png';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default [image1, image2, image3, image4, image5, image6];
+export default [image1, image2, image3, image4, image5, image6, image7];
